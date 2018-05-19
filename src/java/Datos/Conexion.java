52e1f3addb437c4d70538b5edb1bafdfbecf72ff
Daniellecaalle <daniellecalle@hotmail.com>
2018-05-18 22:13:30 -0500

    adicionar proyecto

---
+++ src/java/Datos/Conexion.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Datos;
+
+import java.sql.*;
+
+/**
+ *
+ * @author daniel
+ */
+public class Conexion {
+
+    private Statement stm;
+    private ResultSet rs;
+    private Connection conexion;
+
+    public Connection Conectar() throws ClassNotFoundException {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conexion = DriverManager.getConnection("jdbc:sqlserver://DANIELLE:1433;"
+                    + "databaseName=VENTAS", "sa", "RooT123456");
+            return conexion;
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.toString());
+            return null;
+        }
+    }
+
+    public void cerrarConexion() {
+        try {
+            conexion.close();
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.toString());
+        }
+    }
+
+    public int operacion(String conSQL) {
+
+        int fila = 0;
+
+        try {
+            stm = conexion.createStatement();
+            fila = stm.executeUpdate(conSQL);
+            return fila;
+        } catch (SQLException e) {
+            return fila;
+        }
+    }
+
+    public ResultSet consular(String conSql) {
+        try {
+            stm = conexion.createStatement();
+            rs = stm.executeQuery(conSql);
+            return rs;
+        } catch (SQLException e) {
+            return null;
+        }
+
+    }
+}
